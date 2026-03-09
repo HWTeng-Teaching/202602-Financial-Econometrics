2b507535895742d2516fcbd3e38791e0d4c84a8c
stephen-cyber-3030 <k0909679125@gmail.com>
2026-03-09 23:27:27 +0800

    Update HW2.22.R

--- 35_Stephen/HW0303/HW2.22.R
+++ 35_Stephen/HW0303/HW2.22.R
@@ -1,3 +1,5 @@
+#2.22
+
 rm(list=ls())
 remotes::install_github("ccolonescu/POE5Rdata", force = TRUE)
 library(POE5Rdata)
@@ -6,26 +8,26 @@ library(stargazer)
 data('star5_small')
 head(star5_small)
 
-#2.22.a
+#2.22 (a)
 table_22_a <- lm(totalscore ~ small, data = star5_small)
 summary(table_22_a)
 
-#2.22.b1
+#2.22 (b1)
 table_22_b1 <- lm(readscore ~ small, data = star5_small)
 summary(table_22_b1)
 
-#2.22.b2
+#2.22 (b2)
 table_22_b2 <- lm(mathscore ~ small, data = star5_small)
 summary(table_22_b2)
 
-#2.22.c
+#2.22 (c)
 table_22_c <- lm(totalscore ~ aide, data = star5_small)
 summary(table_22_c)
 
-#2.22.d1
+#2.22 (d1)
 table_22_d1 <- lm(readscore ~ aide, data = star5_small)
 summary(table_22_d1)
 
-#2.22.d2
+#2.22 (d2)
 table_22_d2 <- lm(mathscore ~ aide, data = star5_small)
 summary(table_22_d2)
