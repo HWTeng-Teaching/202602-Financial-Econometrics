8a94f8cd22aad8e9cb5cc400670e9ad84fe6e491
stephen-cyber-3030 <k0909679125@gmail.com>
2026-03-09 23:24:14 +0800

    Add files via upload

---
+++ 35_Stephen/HW0303/HW2.25(c)(d).R
@@ -0,0 +1,15 @@
+
+library(POE5Rdata)
+data("cex5_small")
+
+#2.25(c)
+cex5_small$lnfoodaway <- log(cex5_small$foodaway)
+cex5_small$lnfoodaway[is.infinite(cex5_small$lnfoodaway)] <- NA
+
+hist(cex5_small$lnfoodaway)
+
+summary(cex5_small$lnfoodaway)
+
+#2.25(d)
+lm_Income_lnFoodaway <- lm(lnfoodaway ~ income, data = cex5_small)
+summary(lm_Income_lnFoodaway)
---
+++ 35_Stephen/HW0303/HW2.25(e)(f).R
@@ -0,0 +1,16 @@
+library(POE5data)
+data("cex5_small")
+
+cex5_small$lnfoodaway <- log(cex5_small$foodaway)
+cex5_small$lnfoodaway[is.infinite(cex5_small$lnfoodaway)] <- NA
+regmod3 <- lm(lnfoodaway~income, data = cex5_small, , na.action = na.exclude)
+
+#2.25 (e)
+plot(cex5_small$income, cex5_small$lnfoodaway, xlab = "Income",ylab = "ln(foodaway)",main = "Scatterplot + Fitted Line")
+intercept <- coef(regmod3)[1]
+slope <- coef(regmod3)[2]
+abline(a = intercept, b = slope, col = "red",lwd = 3)
+
+#2.25 (f)
+plot(resid(regmod3) ~income, data = cex5_small)
+resid(regmod3)
