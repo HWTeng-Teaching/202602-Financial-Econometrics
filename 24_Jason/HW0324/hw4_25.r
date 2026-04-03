da00853799e07d8e56d1acb9788b810060f0c853
HWfjt <hinatataro.mg14@nycu.edu.tw>
2026-04-03 20:38:32 +0800

    Update hw4_25.r

--- 24_Jason/HW0324/hw4_25.r
+++ 24_Jason/HW0324/hw4_25.r
@@ -1,11 +1,10 @@
 library(POE5Rdata)
-library(tseries)
 data("collegetown")
 
 # setup
-model_loglinear <- lm(log(price) ~ sqft,       data = collegetown)
-model_loglog    <- lm(log(price) ~ log(sqft),  data = collegetown)
-model_linear    <- lm(price ~ sqft,            data = collegetown)
+model_loglinear <- lm(log(price) ~ sqft,      data = collegetown)
+model_loglog    <- lm(log(price) ~ log(sqft), data = collegetown)
+model_linear    <- lm(price ~ sqft,           data = collegetown)
 
 mean_sqft  <- mean(collegetown$sqft)
 mean_price <- mean(collegetown$price)
@@ -29,14 +28,12 @@ cat("Slope at means:", alpha2 * (mean_price / mean_sqft), "\n")
 cat("Elasticity (alpha2):", alpha2, "\n")
 
 #(c)
-# R^2
 cat("Log-linear R^2:", summary(model_loglinear)$r.squared, "\n")
 cat("Log-log R^2:",    summary(model_loglog)$r.squared, "\n")
 cat("Linear R^2:",     summary(model_linear)$r.squared, "\n")
 
-# Generalized R^2
 gen_r2 <- function(model, log_y = TRUE) {
-  y <- collegetown$price
+  y     <- collegetown$price
   y_hat <- if (log_y) exp(fitted(model)) else fitted(model)
   cor(y, y_hat)^2
 }
@@ -53,9 +50,18 @@ hist(res_loglinear, main = "Residuals: Log-Linear", xlab = "Residuals", breaks =
 hist(res_loglog,    main = "Residuals: Log-Log",    xlab = "Residuals", breaks = 30)
 hist(res_linear,    main = "Residuals: Linear",     xlab = "Residuals", breaks = 30)
 
-jarque.bera.test(res_loglinear)
-jarque.bera.test(res_loglog)
-jarque.bera.test(res_linear)
+jb_test <- function(x) {
+  n  <- length(x)
+  s  <- mean((x - mean(x))^3) / mean((x - mean(x))^2)^1.5   # skewness
+  k  <- mean((x - mean(x))^4) / mean((x - mean(x))^2)^2 - 3 # excess kurtosis
+  jb <- n / 6 * (s^2 + k^2 / 4)
+  pv <- pchisq(jb, df = 2, lower.tail = FALSE)
+  cat("JB =", jb, "  p-value =", pv, "\n")
+}
+
+cat("Log-linear: "); jb_test(res_loglinear)
+cat("Log-log: ");    jb_test(res_loglog)
+cat("Linear: ");     jb_test(res_linear)
 
 #(e)
 plot(collegetown$sqft, res_loglinear,
@@ -99,5 +105,5 @@ pred_interval <- function(model, x0, log_y = TRUE, log_x = FALSE) {
 }
 
 cat("Log-linear PI:", pred_interval(model_loglinear, sqft_f), "\n")
-cat("Log-log PI:",    pred_interval(model_loglog,    sqft_f, log_y = TRUE, log_x = TRUE), "\n")
-cat("Linear PI:",     pred_interval(model_linear,    sqft_f, log_y = FALSE), "\n")
+cat("Log-log PI:",    pred_interval(model_loglog, sqft_f, log_y = TRUE, log_x = TRUE), "\n")
+cat("Linear PI:",     pred_interval(model_linear, sqft_f, log_y = FALSE), "\n")
