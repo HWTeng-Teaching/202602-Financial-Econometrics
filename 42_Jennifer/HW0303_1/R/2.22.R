8aedea9de8981e8ea12bba5350395af5e885b53b
ChienHsinYi <syjean008@gmail.com>
2026-04-07 21:49:48 +0800

    Add files via upload

---
+++ 42_Jennifer/HW0303_1/R/2.22.R
@@ -0,0 +1,24 @@
+#2.22a
+#載入資料套件
+library(POE5Rdata)
+#建立迴歸模型
+#linear model（線性迴歸）
+#data = star5_small 資料來源
+mod1<-lm(totalscore~small,data=star5_small) 
+smod1<-summary(mod1)
+smod1
+mod2<-lm(readscore~small,data= star5_small)
+smod2<-summary(mod2)
+smod2
+mod3<-lm(mathscore~small,data=star5_small)
+smod3<-summary(mod3)
+smod3
+mod4<-lm(totalscore~aide,data=star5_small)
+smod4<-summary(mod4)
+smod4
+mod5<-lm(readscore~aide,data=star5_small)
+smod5<-summary(mod5)
+smod5
+mod6<-lm(mathscore~aide,data=star5_small)
+smod6<-summary(mod6)
+smod6
