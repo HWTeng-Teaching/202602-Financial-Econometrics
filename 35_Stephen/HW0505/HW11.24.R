41ece6ce8f83464eef51220e991b652be8e6d535
stephen-cyber-3030 <k0909679125@gmail.com>
2026-05-12 21:27:05 +0800

    Update HW11.24.R

--- 35_Stephen/HW0505/HW11.24.R
+++ 35_Stephen/HW0505/HW11.24.R
@@ -5,24 +5,24 @@ library(AER)
 library(car)
 data = fultonfish
 
-#11.24(a)
+#(a)
 rf_price <- lm(lprice ~ mon + tue + wed + thu + stormy + mixed, data = fultonfish)
 # check p-value of mixed
 coef(summary(rf_price))
 # check the joint significance of STORMY and MIXED
 linearHypothesis(rf_price, c("stormy = 0", "mixed = 0"))
 
-#11.24(b)
+#(b)
 demand_iv <- ivreg(lquan ~ lprice + mon + tue + wed + thu |
     mon + tue + wed + thu + stormy + mixed,data = fultonfish)
 coef(summary(demand_iv))
 
-#11.24(c)
+#(c)
 u_hat <- resid(demand_iv)
 sargan_aux <- lm(u_hat ~ mon + tue + wed + thu + stormy + mixed, data = fultonfish)
 S <- nobs(demand_iv) * summary(sargan_aux)$r.squared
 p_value <- pchisq(S, df = 1, lower.tail = FALSE)
 round(c(Sargan = S, df = 1, p.value = p_value), 4)
 
-#11.24(d)
+#(d)
 linearHypothesis(rf_price, c("mon = 0", "tue = 0", "wed = 0", "thu = 0"))
